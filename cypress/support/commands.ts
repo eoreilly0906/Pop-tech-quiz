b26114be6b0677e01f39d15c01af39d945eac706
Edward O <edwardoreilly0906@gmail.com>
2025-04-28 15:34:38 -0500

    Remove Cypress end-to-end tests for the Tech Quiz component to streamline testing strategy and focus on essential functionalities.

---
+++ cypress/support/commands.ts
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      // Add custom command types here
+    }
+  }
+}
+
+// Example custom command:
+// Cypress.Commands.add('customCommand', () => {
+//   // Command implementation
+// })
+
+export {} 
